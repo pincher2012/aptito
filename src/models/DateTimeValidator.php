7882b3bab825bd5d5e468a2f743ac4d5cc4ed0bc
Alexander Busygin <pincher2012@yandex.ru>
2016-11-05 18:12:48 +0500

    Реализован валидатор даты

--- src/models/DateTimeValidator.php
+++ src/models/DateTimeValidator.php
@@ -16,6 +16,12 @@ class DateTimeValidator
      */
     public function validate($date)
     {
+        if ($date === null) {
+            return true;
+        }
 
+        $filterResult = filter_var($date, FILTER_VALIDATE_INT, ['options' => ['min_range' => 0]]);
+
+        return $filterResult !== false;
     }
 }
